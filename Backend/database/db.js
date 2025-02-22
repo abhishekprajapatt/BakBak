28d0168cca12517c66da48ddf9b566943ccfc271
Abhishek Prajapatt <145788814+abhishekprajapati2006@users.noreply.github.com>
2025-02-22 17:32:23 +0530

    create backend schema

---
+++ Backend/database/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connetDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Database Connected Successfully...!');
+    } catch (error) {
+        console.log('Database Not Connected...!');
+    }
+}
+
+export default connetDB;
