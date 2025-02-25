43b9589b2809beaa8add5242149775eb63d21300
Abhishek Prajapatt <145788814+abhishekprajapati2006@users.noreply.github.com>
2025-02-25 15:09:26 +0530

    write build and start command for create frontend dist file

--- Backend/index.js
+++ Backend/index.js
@@ -5,11 +5,12 @@ import cors from 'cors';
 import connectDB from './database/db.js';
 import userRouter from './routes/user.route.js';
 import messageRouter from './routes/message.route.js';
+import path from 'path';
 
 dotenv.config({});
-connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,6 +20,13 @@ app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/message', messageRouter);
 
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.get('*', (_, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+});
+
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server listen at port : ${PORT}`);
 });
