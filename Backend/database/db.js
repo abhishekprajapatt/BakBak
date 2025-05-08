801739c9d8f76833c70f745af3dca1a4fbf51eae
Abhishek Prajapatt <145788814+abhishekprajapati2006@users.noreply.github.com>
2025-05-08 23:45:34 +0530

    some issue

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -8,5 +8,4 @@ const connectDB = async () => {
         console.log('Database Not Connected...!');
     }
 }
-
 export default connectDB;
--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -8,4 +8,5 @@ router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/logout').get(logout);
 router.route('/').get(isAuthenticated, getOtherUsers);
+
 export default router;
